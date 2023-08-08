de8e8551dd48296e7cb7b9dcd708867eecff4b8c
kaysee <kayseeclintone@gmail.com>
2023-08-08 16:25:43 +0300

    Addwd a new widget

---
+++ widgets/spinbox.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import ttk
+
+# functions
+# window
+app = tk.Tk()
+app.title('A simple spinbox')
+app.geometry('200x100')
+# widget
+# run program
+app.mainloop()
