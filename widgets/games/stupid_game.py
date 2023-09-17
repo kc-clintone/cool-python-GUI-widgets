cb6ca4b6c12463aa8245e0bcbf135a49c3af389a
kaysee <kayseeclintone@gmail.com>
2023-09-17 12:28:19 +0300

    changed a few things

--- widgets/games/stupid_game.py
+++ widgets/games/stupid_game.py
@@ -10,8 +10,10 @@ def change_window_position():
 
 def change_text_and_buttons():
     title.config(text='I knew you were stupid.', font=("Helvetica", 12, "bold"))
-    yes_btn.pack_forget()
-    no_btn.pack_forget()
+	container.pack_forget()
+	
+    # yes_btn.pack_forget()
+    # no_btn.pack_forget()
 
 # Create the main window
 app = ttk.Window()
@@ -19,16 +21,19 @@ app.title("Stupid Game")
 app.geometry("200x200")
 app.resizable(False, False)
 
-# Create the label with the text
-title = ttk.Label(app, text="Are you stupid?", font=("Helvetica", 12, "bold"))
-title.pack(pady=20)
-
+container = ttk.Frame(master = app)
 # Create the 'Yes' button
-yes_btn = tk.Button(app, text="Yes", command=change_text_and_buttons)
+yes_btn = tk.Button(container, text="Yes", command=change_text_and_buttons)
 yes_btn.pack(side=tk.LEFT, padx=10)
 
 # Create the 'No' button
-no_btn = tk.Button(app, text="No", command=change_window_position)
+no_btn = tk.Button(container, text="No", command=change_window_position)
 no_btn.pack(side=tk.RIGHT, padx=10)
+container.pack(pady = 15)
+
+
+# Create the label with the text
+title = ttk.Label(app, text="Are you stupid?", font=("Helvetica", 12, "bold"))
+title.pack(pady=20)
 
 app.mainloop()
