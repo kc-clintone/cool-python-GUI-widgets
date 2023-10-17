91c6869a7fb018a74b51ecfe6c1fd4adc64a00bd
kaysee <kayseeclintone@gmail.com>
2023-10-17 19:34:42 +0300

    Fixing bugsss[11]

--- widgets/spinbox/spinbox.py
+++ widgets/spinbox/spinbox.py
@@ -1,18 +1,18 @@
 import tkinter as tk
 from tkinter import ttk
 
-# functions
-# window
+# =====app logic=====
+# =====main window=====
 app = tk.Tk()
 app.title('A simple spinbox')
 app.geometry('200x100')
 
-# widget
+#=====widget=====
 #---Spinbox---
 start_val = tk.IntVar(value=10)
 spin = ttk.Spinbox(app, from_ = 10, to = 50, textvariable=start_val)
 spin.bind('<<Increment>>', lambda event: print('Up'))
 spin.bind('<<Decrement>>', lambda event: print('Down'))
 spin.pack(pady = 7)
-# run program
+# =====run program=====
 app.mainloop()
