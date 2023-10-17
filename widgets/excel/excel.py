df0a111432b6261d42252b2e6977f84e6d0af477
kaysee <kayseeclintone@gmail.com>
2023-10-17 19:21:53 +0300

    Fixing bugsss[6]

--- widgets/excel/excel.py
+++ widgets/excel/excel.py
@@ -2,28 +2,27 @@ import tkinter as tk
 from tkinter import ttk
 from random import choice
 
-#app
+#====app window=====
 app = tk.Tk()
-app.geometry('600x400')
-app.title('Excel_like_app')
+app.geometry('610x410')
+app.title('Fake Excel')
 
-#dummy_data
+#-----dummy_data-----
 f_names = ['Gary', 'Simon', 'Greg', 'David', 'Will', 'Bill', 'Val', 'Gill', 'Bob', 'Anne', 'Jane']
 s_names = ['Merryweather', 'Sympson', 'Hillcrest', 'Goldberg', 'Thompson', 'Wellwill', 'Ambers',
 'Anderson', 'Winterstorm', 'Pitterson', 'Robertson']
 
-#main
+#=====main=====
 excel = ttk.Treeview(app, columns = ('f','s','e'), show = 'headings')
 excel.heading('f', text = 'First Name')
 excel.heading('s', text = 'Second Name')
 excel.heading('e', text = 'Email Addr')
 excel.pack(fill = 'both', expand = 'True')
 
-#events
+#=====events=====
 def _dlt_entry(_):
     for i in excel.selection():
         excel.delete(i)
-    # excel.selection_clear
     print('Item deleted')
 
 def selected_entry(_):
